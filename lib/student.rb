1242de6f98ba0069802364ff48c574075e980950
Emily S <emily.sugano@gmail.com>
2018-06-08 20:55:28 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,7 @@ class Student
       @name = name
       @grade = grade
     else
-      sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?;"
+      sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
       DB[:conn].execute(sql, self.name,self.grade, self.id)
     end
   end
